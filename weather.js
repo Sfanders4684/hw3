44a40e1649c2e9f6d7d38f81413965ea7f4a9b3e
Sean <seanfanderson@macintosh-2.gateway.pace.com>
2018-03-01 20:40:48 -0600

    update

--- weather.js
+++ weather.js
@@ -1,30 +1,56 @@
 let updateWidget = function(data) {
 
-  console.log("Got weather data: ", data)
+  // console.log("Got weather data: ", data)
   // YOUR CODE GOES HERE
 
   // HINT:
   // Weather icons are provided for you. Sample URL: http://openweathermap.org/img/w/01d.png
   // The very last part ('01d.png') should be obtained from the weather information.
+  current_temp = Math.round(data.main.temp)
+  icon = data.weather[0].icon
+  location_name = data.name
 
-
+  $("#weather").children("img").attr("src", "https://openweathermap.org/img/w/".concat(icon, ".png"))
+  $("#weather").children("div").children("p").text("It is ".concat(current_temp, " degrees outside."));
+  $("#weather").children("div").children("h4").text(location_name);
 }
 
 
 let getWeather = function(event) {
-  let latitude = '48.8566';
-  let longitude = '2.3522';
-  let apiKey = 'e38e601062df4841f3932da84610dedb'; // REPLACE THIS VALUE with your own key. - DONE
 
-  let weatherServiceURL = 'https://api.openweathermap.org/data/2.5/weather?'
-  weatherServiceURL += 'lat=' + latitude
-  weatherServiceURL += '&lon=' + longitude
-  weatherServiceURL +='&appid=' + apiKey + '&units=imperial'
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 10000,
+    maximumAge: 0
+  };
 
-  fetch(weatherServiceURL).then(convertToJSON).then(updateWidget).catch(displayError);
+  function success(pos) {
+    var crd = pos.coords;
+    let latitude = crd.latitude;
+    let longitude = crd.longitude;
+    let apiKey = 'e38e601062df4841f3932da84610dedb'; // REPLACE THIS VALUE with your own key. - DONE
+
+    let weatherServiceURL = 'https://api.openweathermap.org/data/2.5/weather?'
+    weatherServiceURL += 'lat=' + latitude
+    weatherServiceURL += '&lon=' + longitude
+    weatherServiceURL +='&appid=' + apiKey + '&units=imperial'
+
+    fetch(weatherServiceURL).then(convertToJSON).then(updateWidget).catch(displayError);
+
+  };
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  };
+
+  navigator.geolocation.getCurrentPosition(success, error, options);
+
 }
 
 
+$("#weather").on("click", "a", function (event) {
+  getWeather();
+});
 
 
 ////////////////////////////////////////////////////////////
